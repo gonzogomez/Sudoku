7c898f3187e30fa0db66185417e8c75b550f7042
Gonzalo Gomez <gonzalogomez@Gonzalos-MacBook-Pro.local>
2013-09-30 00:44:39 -0600

    check row, column, and square are close to being done

--- sudoku.js
+++ sudoku.js
@@ -1,5 +1,35 @@
 var board = new Array(81);
-for (var i = board.length-1; i>=0; --i) board[i] = 10;
+var boardEasy = new Array(81);
+for(var i = boardEasy.length-1; i>=0; --i) boardEasy[i] = "";
+boardEasy[1] = 9;
+boardEasy[3] = 7;
+boardEasy[6] = 8;
+boardEasy[7] = 6;
+boardEasy[10] = 3;
+boardEasy[11] = 1;
+boardEasy[14] = 5;
+boardEasy[16] = 2;
+boardEasy[18] = 8;
+boardEasy[20] = 6;
+boardEasy[29] = 7;
+boardEasy[31] = 5;
+boardEasy[35] = 6;
+boardEasy[39] = 3;
+boardEasy[41] = 7;
+boardEasy[45] = 5;
+boardEasy[49] = 1;
+boardEasy[51] = 7;
+boardEasy[60] = 1;
+boardEasy[62] = 9;
+boardEasy[64] = 2;
+boardEasy[66] = 6;
+boardEasy[69] = 3;
+boardEasy[70] = 5;
+boardEasy[73] = 5;
+boardEasy[74] = 4;
+boardEasy[77] = 8;
+boardEasy[79] = 7;
+
 window.onload = function(){
 	
 	//Create 81 canvases for the 81 squares in the sudoku game
@@ -16,80 +46,160 @@ window.onload = function(){
 		
 		//Make some HTML5 modifications to each canvas
 		var ctx=c.getContext('2d');								//Get the context - needed for HTML5 manipulation
+
 		ctx.fillStyle='#FFFFFF';								//Make it blank to begin with
 		ctx.fillRect(0,0,48,48);
 	}
+
+	color_square(0);
+	color_square(6);
+	color_square(54);
+	color_square(30);
+	color_square(60);
+
 	generate_board(15);
 	//Connect the reset button to its function
 	//document.getElementById("reset").onclick = reset;
 }
 
+function color_square(address){
+	console.log("in color_square function");
+	var row = Math.floor(address / 9);
+	var column = address%9;
+
+	for (var i=0; i<3; i++){
+		for(var j=0; j<3; j++){
+			cell = (row + i) * 9 + (column +j);
+			console.log(cell);
+			var c = document.getElementById('board').children[cell];
+			var ctx=c.getContext('2d');
+			ctx.font = "20 Arial";
+			ctx.strokeStyle = "33CCFF";
+			ctx.strokeText(0, 24, 28);
+			ctx.fillStyle='#33CCFF';
+			ctx.fillRect(0,0,48,48);
+		}
+	}
+}
+
 //called when user clicks on square
 function make_move(){
+	var number = prompt("Please enter a number from 1-9");
+	var n = document.getElementById(this.id);
+	boardEasy[n.className] = number;
+	redraw_board();
+}
 
+function redraw_board(){
+	for(i=0;i<81;i++){
+		var c = document.getElementById('board').children[i];
+		var ctx=c.getContext('2d');
+		ctx.fillStyle='#FFFFFF';
+		ctx.fillRect(0,0,48,48);
+	}
+	color_square(0);
+	color_square(6);
+	color_square(54);
+	color_square(30);
+	color_square(60);
+	for(i=0;i<81;i++){
+		var c = document.getElementById('board').children[i];
+		var ctx=c.getContext('2d');
+		ctx.textAlign = 'center';
+		ctx.font = "20 Arial";
+		ctx.strokeStyle = "#FF0000";
+		ctx.strokeText(boardEasy[i], 24, 28);
+	}
 }
 
 // randomly assigns numbers on the board based on difficulty
 function generate_board(count){
-	for(var j=0;j<count;j++){
-		var valid_spot = false;
-		while(valid_spot != true){
-			var num = Math.floor((Math.random()*9)+1);
-			var address = Math.floor((Math.random()*81));
-			// if(validate(num, address)){
-			// 	valid_spot = true;
-			// }
-			valid_spot = validate(num, address);
-			console.log(valid_spot);
-		}
-		var c = document.getElementById('board').children[address];
+	redraw_board();
+	for(var i=0; i < boardEasy.length; i++){
+		var c = document.getElementById('board').children[i];
 		var ctx = c.getContext('2d');
 		ctx.textAlign = 'center';
 		ctx.font = "20 Arial";
 		ctx.strokeStyle = "#FF0000";
-		ctx.strokeText(num, 24, 28);
-		board[address] = num;
-		//console.log("Address = "+ address);
-		//console.log("Array Value = " + board[address]);
+		ctx.strokeText(boardEasy[i], 24, 28);
 	}
+
 }
 
 // returns true if num being place in address is a valid placement
-function validate(num, address){
-	var temp1 = check_row(num, address);
-	var v1 = true;
-	if(temp1){
-		//return true;
-		v1 = true;
+function check_answer(){
+	console.log("In check answer function");
+	var win = false;
+	console.log("right before solution call")
+	win = solution();
+	if (win){
+		alert("Great Job");
 	}
-	else{
-		//return false;
-		v1 = false;
+	else {
+		alert("You blew it");
 	}
-	return v1;
+}
+
+function solution() {
+
+	for (var i=0; i<boardEasy.length; i++){
+		// if ((check_row(boardEasy[i], i) && check_column(boardEasy[i], i) && check_square(boardEasy[i], i)) == false){
+		// 	console.log("in if statment");
+		// 	return false;
+		// }
+		if(check_square(boardEasy[i], i) == false){
+			return false;
+		}
+	}
+	console.log("it made it here");
+	return true;
 }
 
 //checks to see if valid in row
 function check_row(num, address){
 	var row = Math.floor(address / 9);
-	console.log(row);
-	var v = true;
-	for(var i=0; i<9; i++){
+	for (var i=0; i<9; i++){
 		var cell = (row * 9 + i);
-		var x = 10;
-		if(board[cell] == num){
-			//console.log("false");
-			v = false;
+		if ( cell != address){
+			if ((boardEasy[cell] == num) || (boardEasy[cell] == "")){
+				console.log("row "+row);
+				return false;
+			}
 		}
 	}
-	return v;
+	return true;
 }
 
 //check if num occurs in column
 function check_column(num, address){
+	var column = address%9;
+	for (var i=0; i<9; i++){
+		var cell = (column + 9*i);
+		console.log("cell: "+cell);
+		if ( cell != address){
+			if ((boardEasy[cell] == num) || (boardEasy[cell] == "")){
+				console.log("column "+column);
+				return false;
+			}
+		}
+	}
 	return true;
 }
 
 function check_square(num, address){
+	var row = Math.floor(address / 9);
+	var column = address%9;
+
+	for (var i=0; i<3; i++){
+		for(var j=0; j<3; j++){
+			cell = (row + i) * 9 + (column +j);
+			if (cell != address){
+				if((boardEasy[cell] == num) || (boardEasy[cell] == "")){
+					console.log("cell "+ cell);
+					return false;
+				}
+			}
+		}
+	}
 	return true;
 }
